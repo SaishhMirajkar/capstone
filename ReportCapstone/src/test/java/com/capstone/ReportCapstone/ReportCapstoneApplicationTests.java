fce0ceaeb9d846016c70e7c7648216a95c35e8f8
Akshay.R <Akshay.R@BRIAPWLqhYJm7T1.BRILLIO.COM>
2024-05-18 21:14:45 +0530

    Report- Akshay

---
+++ ReportCapstone/src/test/java/com/capstone/ReportCapstone/ReportCapstoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone.ReportCapstone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReportCapstoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
