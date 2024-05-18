fab1e0249a317e460aa2e3e33412d7dcde5aea51
Akshay.R <Akshay.R@BRIAPWLqhYJm7T1.BRILLIO.COM>
2024-05-18 21:04:41 +0530

    Data Ingetion- Akshay

---
+++ DataCapstone/src/main/java/com/capstone/DataCapstone/DataCapstoneApplication.java
@@ -0,0 +1,13 @@
+package com.capstone.DataCapstone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataCapstoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataCapstoneApplication.class, args);
+	}
+
+}
