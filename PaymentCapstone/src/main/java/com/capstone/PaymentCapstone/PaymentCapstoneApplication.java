b9f0c935fc48aa416482b09e69c453c565ec2291
Akshay.R <Akshay.R@BRIAPWLqhYJm7T1.BRILLIO.COM>
2024-05-18 21:10:00 +0530

    Payment- Akshay

---
+++ PaymentCapstone/src/main/java/com/capstone/PaymentCapstone/PaymentCapstoneApplication.java
@@ -0,0 +1,13 @@
+package com.capstone.PaymentCapstone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentCapstoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentCapstoneApplication.class, args);
+	}
+
+}
