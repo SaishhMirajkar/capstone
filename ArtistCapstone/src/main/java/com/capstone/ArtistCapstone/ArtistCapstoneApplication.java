e7e0e762d0f0a02d4eefc668ee9ee3addd4fe706
Akshay.R <Akshay.R@BRIAPWLqhYJm7T1.BRILLIO.COM>
2024-05-23 08:50:50 +0530

    Entity reference

--- ArtistCapstone/src/main/java/com/capstone/ArtistCapstone/ArtistCapstoneApplication.java
+++ ArtistCapstone/src/main/java/com/capstone/ArtistCapstone/ArtistCapstoneApplication.java
@@ -2,6 +2,8 @@ package com.capstone.ArtistCapstone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class ArtistCapstoneApplication {
@@ -9,5 +11,9 @@ public class ArtistCapstoneApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ArtistCapstoneApplication.class, args);
 	}
-
+	
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+}
 }
---
+++ Manager/src/main/java/com/capstone/Manager/Entities/ArtistEntity.java
@@ -0,0 +1,41 @@
+package com.capstone.Manager.Entities;
+
+public class ArtistEntity {
+    private int artistId;
+    private long phoneNumber;
+    private String stageName;
+    private String realName;
+
+    // Getters and setters
+    public int getArtistId() {
+        return artistId;
+    }
+
+    public void setArtistId(int artistId) {
+        this.artistId = artistId;
+    }
+
+    public long getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(long phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getStageName() {
+        return stageName;
+    }
+
+    public void setStageName(String stageName) {
+        this.stageName = stageName;
+    }
+
+    public String getRealName() {
+        return realName;
+    }
+
+    public void setRealName(String realName) {
+        this.realName = realName;
+    }
+}
