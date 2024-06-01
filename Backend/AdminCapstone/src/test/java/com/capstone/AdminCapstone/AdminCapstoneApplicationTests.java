af0abc94c3cfab7a4501b35f8f5a629c23310023
Chirag.S <Chirag.S@BRIAPWLSk7Yoeq4.BRILLIO.COM>
2024-06-02 01:48:15 +0530

    AdminCapstone Test Cases 80+% - Chirag S

--- Backend/AdminCapstone/src/test/java/com/capstone/AdminCapstone/AdminCapstoneApplicationTests.java
+++ Backend/AdminCapstone/src/test/java/com/capstone/AdminCapstone/AdminCapstoneApplicationTests.java
@@ -1,13 +1,22 @@
 package com.capstone.AdminCapstone;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class AdminCapstoneApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+ 
+import org.springframework.boot.SpringApplication;
+ 
+class AdminCapstoneApplicationTest {
+ 
+    @Test
+    void main_InvokesSpringApplicationRun() {
+        // Arrange
+        String[] args = {}; // Assuming no command line arguments are passed
+        SpringApplication springApplicationMock = mock(SpringApplication.class);
+ 
+        // Act
+        AdminCapstoneApplication.main(args);
+ 
+        verify(springApplicationMock);
+    }
 }
