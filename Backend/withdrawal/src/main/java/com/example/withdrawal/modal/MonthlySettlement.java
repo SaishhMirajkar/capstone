835998594918aa294ccfd03238817ec6962ef632
Akshay.R <Akshay.R@BRIAPWLqhYJm7T1.BRILLIO.COM>
2024-06-02 10:46:12 +0530

    Withdrawal

---
+++ Backend/withdrawal/src/main/java/com/example/withdrawal/modal/MonthlySettlement.java
@@ -0,0 +1,42 @@
+package com.example.withdrawal.modal;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class MonthlySettlement {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	
+	private int userId ;
+	private double dailyRoyalty;
+	private LocalDate date;
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	
+	public double getDailyRoyalty() {
+		return dailyRoyalty;
+	}
+	public void setDailyRoyalty(double dailyRoyalty) {
+		this.dailyRoyalty = dailyRoyalty;
+	}
+	public LocalDate getDate() {
+		return date;
+	}
+	public void setDate(LocalDate date) {
+		this.date = date;
+	}
+	
+	
+	
+
+}
