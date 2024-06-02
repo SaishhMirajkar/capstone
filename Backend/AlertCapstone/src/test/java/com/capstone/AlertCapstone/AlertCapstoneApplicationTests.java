c9f17760f1a2449c3468d9305aab5a8e2e52a67b
Chirag.S <Chirag.S@BRIAPWLSk7Yoeq4.BRILLIO.COM>
2024-06-02 14:05:02 +0530

    Alert Capstone 70% Coverage (100% in Service and Controller) - Chirag S

--- Backend/AlertCapstone/src/test/java/com/capstone/AlertCapstone/AlertCapstoneApplicationTests.java
+++ Backend/AlertCapstone/src/test/java/com/capstone/AlertCapstone/AlertCapstoneApplicationTests.java
@@ -1,13 +1,21 @@
 package com.capstone.AlertCapstone;
 
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class AlertCapstoneApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void applicationContextLoaded() {
 	}
 
+	@Test
+	public void applicationContextTest() {
+	    AlertCapstoneApplication.main(new String[] {});
+	}
 }
