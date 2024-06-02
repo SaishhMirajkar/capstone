79c79e2e60a1abf018016bba365bff369f172229
Akshay.R <Akshay.R@BRIAPWLqhYJm7T1.BRILLIO.COM>
2024-06-02 11:10:44 +0530

    Update

--- Backend/withdrawal/src/main/java/com/example/withdrawal/BankDetailsRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.withdrawal;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BankDetailsRepo extends JpaRepository<BankDetails, String> {
-	
-	//public BankDetails findByaccountNumber(String accountNo);
-	
-}
