9bf63325ebb6fe6b926dbe61f7f06bb9daaf13d6
Chirag.S <Chirag.S@BRIAPWLSk7Yoeq4.BRILLIO.COM>
2024-06-03 17:05:55 +0530

    Deleted unused services - Chirag S

--- Backend/ArtistCapstone/src/main/java/com/capstone/ArtistCapstone/Repositories/ArtistRepository.java
+++
@@ -1,7 +0,0 @@
-package com.capstone.ArtistCapstone.Repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.capstone.ArtistCapstone.Entities.ArtistEntity;
-
-public interface ArtistRepository extends JpaRepository<ArtistEntity, Integer> {
-}
